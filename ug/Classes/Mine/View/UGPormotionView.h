d5d9d25e3f9d8693e853e10aadce9145eb9b9987
andrew <syou66699@gmail.com>
2019-09-09 21:39:36 +0800

    做完推荐

---
+++ ug/Classes/Mine/View/UGPormotionView.h
@@ -0,0 +1,17 @@
+//
+//  UGPormotionView.h
+//  ug
+//
+//  Created by ug on 2019/9/9.
+//  Copyright © 2019 ug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UGPormotionView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
