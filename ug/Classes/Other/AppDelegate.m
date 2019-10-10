6978eb6e74f755d80b45d21476ec14b00a81c14c
xionghx <xionghuanxin@hotmail.com>
2019-10-10 20:07:20 +0800

    badgeNumber set 0

--- ug/Classes/Other/AppDelegate.m
+++ ug/Classes/Other/AppDelegate.m
@@ -184,6 +184,8 @@
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
+	[application setApplicationIconBadgeNumber:0];
+
 	// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 }
 
