97e02e2b6738432fc538242668132cc789fde1ca
andrew <syou66699@gmail.com>
2019-10-28 12:12:29 +0800

    六合

---
+++ ug/Classes/Home/View/首页/六合页面/六合首页/UGLHHomeLotteryCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  UGLHHomeLotteryCollectionViewCell.m
+//  ug
+//
+//  Created by ug on 2019/10/27.
+//  Copyright © 2019 ug. All rights reserved.
+//
+
+#import "UGLHHomeLotteryCollectionViewCell.h"
+
+@implementation UGLHHomeLotteryCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
