9e30b0c57871ffcbd10d39c46df22020558a991c
andrew <syou66699@gmail.com>
2019-10-30 16:44:15 +0800

    去掉调试

--- ug/Classes/Home/Controller/UGHomeViewController.m
+++ ug/Classes/Home/Controller/UGHomeViewController.m
@@ -1380,18 +1380,18 @@
     }
 }
 - (IBAction)goYOUHUIVC:(id)sender {
-//    BOOL isLogin = UGLoginIsAuthorized();
-//    if (isLogin) {
-//        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Promote" bundle:nil];
-//        UGPromotionsController *qdwebVC = [storyboard instantiateViewControllerWithIdentifier:@"UGPromotionsController"];
-//        [self.navigationController pushViewController:qdwebVC  animated:YES];
-//    }
-//    else {
-//         SANotificationEventPost(UGNotificationShowLoginView, nil);
-//    }
+    BOOL isLogin = UGLoginIsAuthorized();
+    if (isLogin) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Promote" bundle:nil];
+        UGPromotionsController *qdwebVC = [storyboard instantiateViewControllerWithIdentifier:@"UGPromotionsController"];
+        [self.navigationController pushViewController:qdwebVC  animated:YES];
+    }
+    else {
+         SANotificationEventPost(UGNotificationShowLoginView, nil);
+    }
     
-    UGfinancialViewViewController *vc = [UGfinancialViewViewController new];
-    [self.navigationController pushViewController:vc  animated:YES];
+//    UGfinancialViewViewController *vc = [UGfinancialViewViewController new];
+//    [self.navigationController pushViewController:vc  animated:YES];
    
 }
 - (IBAction)historyAcion:(id)sender {
