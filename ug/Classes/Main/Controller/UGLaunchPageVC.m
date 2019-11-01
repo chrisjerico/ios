5abf244f7d7f7ce0e0808af573eaefdd34c67209
fish <gold.fish.a1b1@gmail.com>
2019-11-01 22:06:01 +0800

    更新了 启动页图片加载逻辑；

--- ug/Classes/Main/Controller/UGLaunchPageVC.m
+++ ug/Classes/Main/Controller/UGLaunchPageVC.m
@@ -24,25 +24,30 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	self.view.backgroundColor = UIColor.whiteColor;
-	UIImageView * imageView = [UIImageView new];
-	
-	[CMNetwork.manager requestWithMethod:[[NSString stringWithFormat:@"%@/wjapp/api.php?c=system&a=launchImages", baseServerUrl] stringToRestfulUrlWithFlag:RESTFUL]
-								  params:nil
-								   model:CMResultArrayClassMake(LaunchPageModel.class)
-									post:NO
-							  completion:^(CMResult<id> *model, NSError *err) {
-		NSArray<LaunchPageModel *> * launchPics = model.data;
-		[[NSUserDefaults standardUserDefaults] setObject: launchPics.firstObject.pic forKey:@"launchImage"];
+	__block UIImageView *__imageView = [UIImageView new];
+    
+	[CMNetwork.manager requestWithMethod:[[NSString stringWithFormat:@"%@/wjapp/api.php?c=system&a=launchImages", baseServerUrl] stringToRestfulUrlWithFlag:RESTFUL] params:nil model:CMResultArrayClassMake(LaunchPageModel.class) post:NO completion:^(CMResult<id> *model, NSError *err) {
+        if (!err) {
+            NSArray<LaunchPageModel *> * launchPics = model.data;
+            [[NSUserDefaults standardUserDefaults] setObject:launchPics.firstObject.pic forKey:@"launchImage"];
+            [__imageView sd_setImageWithURL:[NSURL URLWithString:launchPics.firstObject.pic] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+                __imageView.backgroundColor = [UIColor clearColor];
+                NSLog(@"启动图加载%@！", error ? @"失败" : @"成功");
+            }];
+        }
 	}];
 	
-	[self.view addSubview:imageView];
-	[imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+	[self.view addSubview:__imageView];
+	[__imageView mas_makeConstraints:^(MASConstraintMaker *make) {
 		make.edges.equalTo(self.view);
 	}];
-	NSString * picURL = [[NSUserDefaults standardUserDefaults]  valueForKey:@"launchImage"];
-	[imageView sd_setImageWithURL: [NSURL URLWithString:picURL]];
+	NSString *picURL = [[NSUserDefaults standardUserDefaults] valueForKey:@"launchImage"];
+    [__imageView sd_setImageWithURL:[NSURL URLWithString:picURL] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+        __imageView.backgroundColor = [UIColor clearColor];
+        NSLog(@"启动图加载%@！", error ? @"失败" : @"成功");
+    }];
 	
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         APP.Window.rootViewController = [[UGTabbarController alloc] init];
 	});
 }
