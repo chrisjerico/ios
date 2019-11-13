4d45f46776e9a0ab36223b96c0427933a6a1ac2b
andrew <syou66699@gmail.com>
2019-11-13 16:46:34 +0800

    修改viewdidApear 的报错 的bug

--- ug/Classes/Skin/UGSkinManagers+Lottery.m
+++ ug/Classes/Skin/UGSkinManagers+Lottery.m
@@ -40,63 +40,66 @@
                               UGFC3DLotteryController.class,
                               UGPK10NNLotteryController.class,
         ];
-        [UIViewController aspect_hookSelector:@selector(viewWillAppear:) withOptions:AspectPositionAfter usingBlock:^(id <AspectInfo> aspectInfo, BOOL animated ) {
-//                   NSLog(@"%@", aspectInfo);
-//                   NSLog(@"%d", animated);
+        
+         for (Class cls in clsArray) {
+             [cls aspect_hookSelector:@selector(viewWillAppear:) withOptions:AspectPositionAfter usingBlock:^(id <AspectInfo> aspectInfo, BOOL animated ) {
 
-            if ([clsArray containsObject:[aspectInfo.instance class]]) {
-                 NSLog(@"%@-->:%@", @"Appear 下注界面:😜😜😜", NSStringFromClass([aspectInfo.instance class]));
-                UIViewController *vc = (UIViewController *)aspectInfo.instance;
-                FastSubViewCode(vc.view);
-                if ([Skin1.skitType isEqualToString:@"黑色模板"]) {
-                    vc.view.backgroundColor =  Skin1.bgColor;
-                    [subView(@"上背景View") setBackgroundColor:Skin1.bgColor];
-                    [subLabel(@"期数label") setTextColor:Skin1.textColor1];
-                    [subLabel(@"聊天室label") setTextColor:Skin1.textColor1];
-                    [subLabel(@"线label") setBackgroundColor:Skin1.textColor1];
-                    [subView(@"中间view") setBackgroundColor:Skin1.bgColor];
-                    [[vc valueForKey:@"nextIssueLabel"] setTextColor:Skin1.textColor1];
-                    [[vc valueForKey:@"closeTimeLabel"] setTextColor:Skin1.textColor1];
-                    [[vc valueForKey:@"openTimeLabel"] setTextColor:Skin1.textColor1];
-                    [subLabel(@"中间线label") setBackgroundColor:Skin1.textColor1];
-                    [[vc valueForKey:@"tableView"] setBackgroundColor:[UIColor clearColor]];
-                    
-                } else {
-                    vc.view.backgroundColor =  [UIColor whiteColor];
-                    [subView(@"上背景View") setBackgroundColor: [UIColor whiteColor]];
-                    [subLabel(@"期数label") setTextColor: [UIColor blackColor]];
-                    [subLabel(@"聊天室label") setTextColor:[UIColor blackColor]];
-                    [subLabel(@"线label") setBackgroundColor:[UIColor lightGrayColor]];
-                    [[vc valueForKey:@"nextIssueLabel"] setTextColor:[UIColor blackColor]];
-                    [[vc valueForKey:@"closeTimeLabel"] setTextColor:[UIColor blackColor]];
-                    [[vc valueForKey:@"openTimeLabel"] setTextColor:[UIColor blackColor]];
-                    [subLabel(@"中间线label") setBackgroundColor:[UIColor lightGrayColor]];
-                    [[vc valueForKey:@"tableView"] setBackgroundColor:[UIColor whiteColor]];
-                }
-                // 处理OpenLabel
-               void (^block1)(id<AspectInfo>) = ^(id<AspectInfo> aspectInfo) {
-                   if ([clsArray containsObject:[aspectInfo.instance class]]) {
-                       UIViewController *vc = (UIViewController *)aspectInfo.instance;
-                       UILabel *openTimeLabel = [vc valueForKey:@"openTimeLabel"];
-                       if ([Skin1.skitType isEqualToString:@"黑色模板"]) {
-                              if (openTimeLabel.text.length) {
-                                  NSMutableAttributedString *abStr = [[NSMutableAttributedString alloc] initWithString:openTimeLabel.text];
-                                  [abStr addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor] range:NSMakeRange(3, openTimeLabel.text.length - 3)];
-                                  openTimeLabel.attributedText = abStr;
-                              }
-                       } else {
-                              if (openTimeLabel.text.length) {
-                                  NSMutableAttributedString *abStr = [[NSMutableAttributedString alloc] initWithString:openTimeLabel.text];
-                                  [abStr addAttribute:NSForegroundColorAttributeName value:Skin1.navBarBgColor range:NSMakeRange(3, openTimeLabel.text.length - 3)];
-                                  openTimeLabel.attributedText = abStr;
-                              }
-                       }
-                   }
-               };
-              [[aspectInfo.instance class] aspect_hookSelector:@selector(updateOpenLabel ) withOptions:AspectPositionAfter usingBlock:block1 error:nil];
-            }
-        }
-        error:NULL];
+
+                        if ([clsArray containsObject:[aspectInfo.instance class]]) {
+                             NSLog(@"%@-->:%@", @"Appear 下注界面:😜😜😜", NSStringFromClass([aspectInfo.instance class]));
+                            UIViewController *vc = (UIViewController *)aspectInfo.instance;
+                            FastSubViewCode(vc.view);
+                            if ([Skin1.skitType isEqualToString:@"黑色模板"]) {
+                                vc.view.backgroundColor =  Skin1.bgColor;
+                                [subView(@"上背景View") setBackgroundColor:Skin1.bgColor];
+                                [subLabel(@"期数label") setTextColor:Skin1.textColor1];
+                                [subLabel(@"聊天室label") setTextColor:Skin1.textColor1];
+                                [subLabel(@"线label") setBackgroundColor:Skin1.textColor1];
+                                [subView(@"中间view") setBackgroundColor:Skin1.bgColor];
+                                [[vc valueForKey:@"nextIssueLabel"] setTextColor:Skin1.textColor1];
+                                [[vc valueForKey:@"closeTimeLabel"] setTextColor:Skin1.textColor1];
+                                [[vc valueForKey:@"openTimeLabel"] setTextColor:Skin1.textColor1];
+                                [subLabel(@"中间线label") setBackgroundColor:Skin1.textColor1];
+                                [[vc valueForKey:@"tableView"] setBackgroundColor:[UIColor clearColor]];
+                                
+                            } else {
+                                vc.view.backgroundColor =  [UIColor whiteColor];
+                                [subView(@"上背景View") setBackgroundColor: [UIColor whiteColor]];
+                                [subLabel(@"期数label") setTextColor: [UIColor blackColor]];
+                                [subLabel(@"聊天室label") setTextColor:[UIColor blackColor]];
+                                [subLabel(@"线label") setBackgroundColor:[UIColor lightGrayColor]];
+                                [[vc valueForKey:@"nextIssueLabel"] setTextColor:[UIColor blackColor]];
+                                [[vc valueForKey:@"closeTimeLabel"] setTextColor:[UIColor blackColor]];
+                                [[vc valueForKey:@"openTimeLabel"] setTextColor:[UIColor blackColor]];
+                                [subLabel(@"中间线label") setBackgroundColor:[UIColor lightGrayColor]];
+                                [[vc valueForKey:@"tableView"] setBackgroundColor:[UIColor whiteColor]];
+                            }
+                            // 处理OpenLabel
+                           void (^block1)(id<AspectInfo>) = ^(id<AspectInfo> aspectInfo) {
+                               if ([clsArray containsObject:[aspectInfo.instance class]]) {
+                                   UIViewController *vc = (UIViewController *)aspectInfo.instance;
+                                   UILabel *openTimeLabel = [vc valueForKey:@"openTimeLabel"];
+                                   if ([Skin1.skitType isEqualToString:@"黑色模板"]) {
+                                          if (openTimeLabel.text.length) {
+                                              NSMutableAttributedString *abStr = [[NSMutableAttributedString alloc] initWithString:openTimeLabel.text];
+                                              [abStr addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor] range:NSMakeRange(3, openTimeLabel.text.length - 3)];
+                                              openTimeLabel.attributedText = abStr;
+                                          }
+                                   } else {
+                                          if (openTimeLabel.text.length) {
+                                              NSMutableAttributedString *abStr = [[NSMutableAttributedString alloc] initWithString:openTimeLabel.text];
+                                              [abStr addAttribute:NSForegroundColorAttributeName value:Skin1.navBarBgColor range:NSMakeRange(3, openTimeLabel.text.length - 3)];
+                                              openTimeLabel.attributedText = abStr;
+                                          }
+                                   }
+                               }
+                           };
+                          [[aspectInfo.instance class] aspect_hookSelector:@selector(updateOpenLabel ) withOptions:AspectPositionAfter usingBlock:block1 error:nil];
+                        }
+                    }
+                    error:NULL];
+         }
+       
     });
 }
 
