e08cef004535cf96bcee58a9df4bebfeb11448b2
ugtmac <syou66699@gmail.com>
2019-12-24 19:48:16 +0800

    c011   更换成c011hy

--- ug/Classes/Other/SiteModel.m
+++ ug/Classes/Other/SiteModel.m
@@ -108,7 +108,7 @@
             site(@"c091",        @"c091hy",     @"137",     @"io.fhpt.H5691A751",       @"https://83f9.com",                            @"企业包",     @"凤凰彩票"),
             site(@"c084",        @"c084hy",     @"136",     @"io.fhpt.H599DB7B9",       @"https://papghawshugposwaughwsoohu.com",       @"企业包",     @"新2彩票"),//极光 jiguangx2c@gmail.com Qwe741123
             site(@"c049",        @"c049hy",     @"190",     @"com.xinshiji2.yuansheng1", @"https://93922app.com",                        @"企业包",     @"新世纪Ⅱ"),
-            site(@"c011",        @"c011",       @"84",      @"io.fhpt.H599FF71F",       @"https://www.hx627.com",                       @"企业包",     @"华夏彩票"), // 已上架
+            site(@"c011",        @"c011hy",       @"184",      @"io.fhpt.H599FF71F",       @"https://www.hx627.com",                       @"企业包",     @"华夏彩票"), // 已上架
             site(@"c012",        @"c012hy",     @"170",     @"io.fhpt.H5F0B8A01",       @"https://20180849.com",                        @"企业包",     @"8号彩票"),
             site(@"c022",        @"",           @"",        @"io.fhpt.H53EC1170",       @"",                                            @"企业包",     @"68彩票"),
             site(@"c039",        @"c039hy",     @"150",     @"io.fhpt.H5C117128",       @"",                                            @"企业包",     @"汇丰彩票"),
