5fab944a5d84a904a66e2fee5726ce480274a917
fish <gold.fish.a1b1@gmail.com>
2019-12-31 14:54:35 +0800

    提交 node_modules；

---
+++ node_modules/code-push/script/types.d.ts
@@ -0,0 +1,18 @@
+export { AccessKeyRequest, Account, App, AppCreationRequest, CollaboratorMap, CollaboratorProperties, Deployment, DeploymentMetrics, Package, PackageInfo, AccessKey as ServerAccessKey, UpdateMetrics } from "rest-definitions";
+export interface CodePushError {
+    message: string;
+    statusCode: number;
+}
+export interface AccessKey {
+    createdTime: number;
+    expires: number;
+    name: string;
+    key?: string;
+}
+export interface Session {
+    loggedInTime: number;
+    machineName: string;
+}
+export declare type Headers = {
+    [headerName: string]: string;
+};
