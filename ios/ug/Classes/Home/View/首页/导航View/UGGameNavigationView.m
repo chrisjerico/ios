b410ea7605613954780a4b24690fcfd9eecb75ea
xionghx <xionghuanxin@hotmail.com>
2020-01-06 11:37:33 +0800

    #917# c190首页中间游戏导航需增加logo图标，游戏导航栏可进行滑动

--- ios/ug/Classes/Home/View/首页/导航View/UGGameNavigationView.m
+++ ios/ug/Classes/Home/View/首页/导航View/UGGameNavigationView.m
@@ -10,11 +10,34 @@
 #import "YYWebImage.h"
 
 @interface UGGameNavigationView()<UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
-
+@property(nonatomic, strong)UIButton * scrollRightButton;
 @end
 
 @implementation UGGameNavigationView
+-(UIButton *)scrollRightButton {
+	if (!_scrollRightButton) {
+		_scrollRightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+		_scrollRightButton.backgroundColor = [UIColor colorWithHex:0x4981de];
+		[_scrollRightButton setImage: [[UIImage imageNamed:@"jiantouyou"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateNormal];
+		[_scrollRightButton setTintColor:UIColor.whiteColor];
+		[_scrollRightButton addTarget:self action:@selector(scrollRight)];
+		
+	}
+	return _scrollRightButton;
+}
+- (void)scrollRight {
+	
+	NSInteger FrameWidth = UGScreenW - 40;
+	NSInteger contentWidth = self.sourceData.count > 5 ?  self.sourceData.count * (FrameWidth/5) : FrameWidth;
 
+	NSInteger oldContentOffSet_X = self.contentOffset.x;
+	NSInteger distance = contentWidth - FrameWidth;
+	if (distance > 0) {
+		CGFloat newContenOffSet_X = (oldContentOffSet_X + (NSInteger)(distance)/_sourceData.count) % distance;
+		[self setContentOffset:CGPointMake(newContenOffSet_X, 0)];
+
+	}
+}
 - (void)setSourceData:(NSArray<GameModel *> *)sourceData {
 	_sourceData = sourceData;
 	[self reloadData];
@@ -24,7 +47,13 @@
 {
 	UICollectionViewFlowLayout *layout = ({
 		layout = [[UICollectionViewFlowLayout alloc] init];
-		layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+		if ([@"c190" containsString:APP.SiteId]) {
+			layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+
+		} else {
+			layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+
+		}
 		layout;
 	});
 	self = [super initWithFrame:frame collectionViewLayout:layout];
@@ -32,16 +61,37 @@
 		[self registerClass: [UGGameNavigationViewCell class] forCellWithReuseIdentifier:@"UGGameNavigationViewCell"];
 		self.delegate = self;
 		self.dataSource = self;
-		self.scrollEnabled = false;
 	}
 	return self;
 }
-
+- (void)didMoveToWindow {
+	[super didMoveToWindow];
+	
+	[self.superview addSubview: self.scrollRightButton];
+	[self.scrollRightButton mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.centerY.equalTo(self);
+		make.right.equalTo(self);
+		make.width.equalTo(@15);
+		make.height.equalTo(@40);
+	}];
+	UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(0, 0, 15, 40) byRoundingCorners:UIRectCornerTopLeft | UIRectCornerBottomLeft cornerRadii:CGSizeMake(3,3)];
+	CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+	maskLayer.frame = CGRectMake(0, 0, 15, 40);
+	maskLayer.path = maskPath.CGPath;
+	_scrollRightButton.layer.mask = maskLayer;
+	
+	if ([@"c190, test20" containsString:APP.SiteId]) {
+		[self.scrollRightButton setHidden:false];
+	} else {
+		[self.scrollRightButton setHidden:true];
+	}
+}
 - (void)awakeFromNib {
 	
 	[super awakeFromNib];
-
-	[self initWithFrame:CGRectZero];
+	
+	CGRect frame = CGRectMake(0, 0, UGScreenW, 100);
+	[self initWithFrame:frame];
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
@@ -59,18 +109,18 @@
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    GameModel *gm = self.sourceData[indexPath.item];
-    [NavController1 pushViewControllerWithGameModel:gm];
+	GameModel *gm = self.sourceData[indexPath.item];
+	[NavController1 pushViewControllerWithGameModel:gm];
 }
 
 
 #pragma mark ---- UICollectionViewDelegateFlowLayout
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    CGFloat oneLineCnt = 5;
-    if (_sourceData.count%5 && !(_sourceData.count%4)) {
-        oneLineCnt = 4;
-    }
+	CGFloat oneLineCnt = 5;
+	if (_sourceData.count%5 && !(_sourceData.count%4)) {
+		oneLineCnt = 4;
+	}
 	return (CGSize){(UGScreenW - 40)/MIN(oneLineCnt, self.sourceData.count), 75};
 }
 
@@ -93,8 +143,8 @@
 {
 	YYAnimatedImageView * _iconImage;
 	UILabel * _titleLabel;
-    UIImageView * _hotImage;
-    UILabel *_unreadLabel;
+	UIImageView * _hotImage;
+	UILabel *_unreadLabel;
 }
 
 @end
@@ -105,11 +155,11 @@
 {
 	self = [super initWithFrame:frame];
 	if (self) {
-        _hotImage = [UIImageView new];
+		_hotImage = [UIImageView new];
 		_iconImage = [YYAnimatedImageView new];
-        _iconImage.contentMode = UIViewContentModeScaleAspectFit;
+		_iconImage.contentMode = UIViewContentModeScaleAspectFit;
 		_titleLabel = [UILabel new];
-        _titleLabel.textColor = Skin1.textColor1;
+		_titleLabel.textColor = Skin1.textColor1;
 		_titleLabel.font = [UIFont systemFontOfSize:14];
 		[self addSubview:_iconImage];
 		[_iconImage mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -117,65 +167,65 @@
 			make.centerX.equalTo(self);
 			make.width.height.equalTo(@37);
 		}];
-        
-//        _iconImage.layer.cornerRadius = 20;
-//        _iconImage.layer.masksToBounds = YES;
-        [self addSubview:_hotImage];
-        _hotImage.contentMode = UIViewContentModeScaleAspectFit;
-        [_hotImage mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(self);
-            make.right.equalTo(self);
-            make.width.height.mas_equalTo(27);
-        }];
+		
+		//        _iconImage.layer.cornerRadius = 20;
+		//        _iconImage.layer.masksToBounds = YES;
+		[self addSubview:_hotImage];
+		_hotImage.contentMode = UIViewContentModeScaleAspectFit;
+		[_hotImage mas_makeConstraints:^(MASConstraintMaker *make) {
+			make.top.equalTo(self);
+			make.right.equalTo(self);
+			make.width.height.mas_equalTo(27);
+		}];
 		[self addSubview:_titleLabel];
 		[_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
 			make.centerX.equalTo(self);
 			make.bottom.equalTo(self).offset(-10);
 		}];
-        
-        UILabel *unreadCnt = _unreadLabel = [UILabel new];
-        unreadCnt.textAlignment = NSTextAlignmentCenter;
-        unreadCnt.font = [UIFont systemFontOfSize:12];
-        unreadCnt.内边距 = CGPointMake(1, 0);
-        unreadCnt.textColor = [UIColor whiteColor];
-        unreadCnt.backgroundColor = [UIColor redColor];
-        unreadCnt.layer.cornerRadius = 10;
-        unreadCnt.layer.masksToBounds = true;
-        [self addSubview:unreadCnt];
-        [unreadCnt mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.height.mas_equalTo(20);
-            make.width.mas_greaterThanOrEqualTo(20);
-            make.centerX.equalTo(self).offset(20);
-            make.top.equalTo(self);
-        }];
+		
+		UILabel *unreadCnt = _unreadLabel = [UILabel new];
+		unreadCnt.textAlignment = NSTextAlignmentCenter;
+		unreadCnt.font = [UIFont systemFontOfSize:12];
+		unreadCnt.内边距 = CGPointMake(1, 0);
+		unreadCnt.textColor = [UIColor whiteColor];
+		unreadCnt.backgroundColor = [UIColor redColor];
+		unreadCnt.layer.cornerRadius = 10;
+		unreadCnt.layer.masksToBounds = true;
+		[self addSubview:unreadCnt];
+		[unreadCnt mas_makeConstraints:^(MASConstraintMaker *make) {
+			make.height.mas_equalTo(20);
+			make.width.mas_greaterThanOrEqualTo(20);
+			make.centerX.equalTo(self).offset(20);
+			make.top.equalTo(self);
+		}];
 	}
 	return self;
 }
 
 -(void)setModel:(GameModel *)model {
 	_model = model;
-    
-    // 站内信未读消息数量
-    _unreadLabel.hidden = !(model.seriesId==7 && model.subId==14 && UserI.unreadMsg);
-    _unreadLabel.text = _NSString(@"%d", (int)UserI.unreadMsg);
-    __weak_Obj_(_unreadLabel, __unreadLabel);
-    [self xw_removeNotificationForName:UGNotificationGetUserInfoComplete];
-    [self xw_addNotificationForName:UGNotificationGetUserInfoComplete block:^(NSNotification * _Nonnull noti) {
-        __unreadLabel.hidden = !(model.seriesId==7 && model.subId==14 && UserI.unreadMsg);
-        __unreadLabel.text = _NSString(@"%d", (int)UserI.unreadMsg);
-    }];
-    
-    // 热门图标
-    __weak_Obj_(_hotImage, __hotImageView);
-    _hotImage.hidden = !model.tipFlag;
-    [_hotImage sd_setImageWithURL:[NSURL URLWithString:model.hotIcon] placeholderImage:[UIImage imageNamed:@"icon_remen"] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
-        if (error)
-            __hotImageView.image = [UIImage imageNamed:@"icon_remen"];
-    }];
-    
-    [_iconImage yy_setImageWithURL:[NSURL URLWithString:model.icon] placeholder:[UIImage imageNamed:@"zwt"]];
-    [_iconImage startAnimating];
-    _titleLabel.text = model.name.length ? model.name : model.title;
+	
+	// 站内信未读消息数量
+	_unreadLabel.hidden = !(model.seriesId==7 && model.subId==14 && UserI.unreadMsg);
+	_unreadLabel.text = _NSString(@"%d", (int)UserI.unreadMsg);
+	__weak_Obj_(_unreadLabel, __unreadLabel);
+	[self xw_removeNotificationForName:UGNotificationGetUserInfoComplete];
+	[self xw_addNotificationForName:UGNotificationGetUserInfoComplete block:^(NSNotification * _Nonnull noti) {
+		__unreadLabel.hidden = !(model.seriesId==7 && model.subId==14 && UserI.unreadMsg);
+		__unreadLabel.text = _NSString(@"%d", (int)UserI.unreadMsg);
+	}];
+	
+	// 热门图标
+	__weak_Obj_(_hotImage, __hotImageView);
+	_hotImage.hidden = !model.tipFlag;
+	[_hotImage sd_setImageWithURL:[NSURL URLWithString:model.hotIcon] placeholderImage:[UIImage imageNamed:@"icon_remen"] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+		if (error)
+			__hotImageView.image = [UIImage imageNamed:@"icon_remen"];
+	}];
+	
+	[_iconImage yy_setImageWithURL:[NSURL URLWithString:model.icon] placeholder:[UIImage imageNamed:@"zwt"]];
+	[_iconImage startAnimating];
+	_titleLabel.text = model.name.length ? model.name : model.title;
 }
 
 
