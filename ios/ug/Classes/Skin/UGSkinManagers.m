4edaec634535e2f0ed26e499addb15c63dac0776
xionghx <xionghuanxin@hotmail.com>
2020-01-18 14:25:42 +0800

    mark

--- ios/ug/Classes/Skin/UGSkinManagers.m
+++ ios/ug/Classes/Skin/UGSkinManagers.m
@@ -1106,7 +1106,7 @@ static UGSkinManagers *__initSkin1 = nil;
 	NSString *skitType = dict[SysConf.mobileTemplateCategory];
 #if DEBUG
 	NSLog(@"============================skitType=%@",skitType);
-	skitType = @"香槟金";
+//	skitType = @"香槟金";
 #endif
 	return [UGSkinManagers allSkin][skitType];
 
