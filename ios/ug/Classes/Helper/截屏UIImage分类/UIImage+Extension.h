cd3c5fb4f972c2dba084b7fb39a186cd3aedeb85
ugtmac <syou66699@gmail.com>
2020-01-19 21:50:19 +0800

    加点击事件

---
+++ ios/ug/Classes/Helper/截屏UIImage分类/UIImage+Extension.h
@@ -0,0 +1,22 @@
+//
+//  UIImage+Extension.h
+//  ug
+//
+//  Created by ug on 2020/1/19.
+//  Copyright © 2020 ug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (Extension)
+/**
+*  "截屏""
+*
+*
+*/
++ (UIImage *)rendImageWithView:(UIView *)view;
+@end
+
+NS_ASSUME_NONNULL_END
--- ios/ug/Classes/Mine/Controller/我的推广/PromotionAdvertisementVC.m
+++ ios/ug/Classes/Mine/Controller/我的推广/PromotionAdvertisementVC.m
@@ -7,9 +7,12 @@
 //
 
 #import "PromotionAdvertisementVC.h"
+#import "UGinviteInfoModel.h"
+#import "SGQRCodeObtain.h"
+#import "UIImage+Extension.h"
 
 @interface PromotionAdvertisementVC ()
-
+@property (nonatomic, strong) UGinviteInfoModel* inviteInfo;
 @end
 
 @implementation PromotionAdvertisementVC
@@ -17,16 +20,107 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self loadData];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)loadData {
+    if ([UGUserModel currentUser].isTest) {
+        return;
+    }
+    NSDictionary *params = @{@"token":[UGUserModel currentUser].sessid};
+    
+    [SVProgressHUD showWithStatus:nil];
+    WeakSelf;
+    [CMNetwork teamInviteInfoWithParams:params completion:^(CMResult<id> *model, NSError *err) {
+        [CMResult processWithResult:model success:^{
+            [SVProgressHUD dismiss];
+            weakSelf.inviteInfo = model.data;
+            NSLog(@"rid = %@",weakSelf.inviteInfo.rid);
+            [weakSelf bindData];
+        } failure:^(id msg) {
+            [SVProgressHUD dismiss];
+        }];
+    }];
+    
 }
-*/
+- (void)bindData {
+    FastSubViewCode(self.view);
+    UIImage * image = [SGQRCodeObtain generateQRCodeWithData:self.inviteInfo.link_i size:160.0];
+    [subImageView(@"二微码ImgV") setImage:image];
+    [subButton(@"备用网址Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"备用网址Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       
+    }];
+    [subButton(@"朋友圈Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"朋友圈Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       
+    }];
+    [subButton(@"微信好友Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"微信好友Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       
+    }];
+    [subButton(@"QQ好友Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"QQ好友Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       
+    }];
+    [subButton(@"QQ空间Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"QQ空间Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       
+    }];
+    [subButton(@"微博Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"微博Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       
+    }];
+    [subButton(@"分享推广图Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"分享推广图Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       
+    }];
+    [subButton(@"复制链接Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"复制链接Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+       pasteboard.string = self.inviteInfo.link_i;
+       [SVProgressHUD showSuccessWithStatus:@"复制成功"];
+    }];
+    [subButton(@"保存推广图Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"保存推广图Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+      //    1.获取一个截图图片
+          UIImage *newImage = [ UIImage rendImageWithView:self.view];
+      //    2.写入相册
+          UIImageWriteToSavedPhotosAlbum(newImage, self, @selector(image:didFinishSavingWithError:contextInfo:), @"134");
+    }];
+    [subButton(@"说明Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"说明Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+       [NavController1 pushViewController:_LoadVC_from_storyboard_(@"PromotionIntroductionVC") animated:true];
+    }];
+    [subButton(@"我的会员Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"我的会员Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+        [NavController1 pushViewController:_LoadVC_from_storyboard_(@"MyPromotionMembersVC") animated:true];
+    }];
+    [subButton(@"会员投注Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"会员投注Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+[NavController1 pushViewController:_LoadVC_from_storyboard_(@"PromotionBetReportVC") animated:true];
+    }];
+    [subButton(@"会员交易Btn") removeAllBlocksForControlEvents:UIControlEventTouchUpInside];
+    [subButton(@"会员交易Btn") addBlockForControlEvents:UIControlEventTouchUpInside block:^(__kindof UIControl *sender) {
+[NavController1 pushViewController:_LoadVC_from_storyboard_(@"PromotionBetRecordVC") animated:true];
+    }];
 
+
+    
+}
+
+#pragma mark 用来监听图片保存到相册的状况
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo{
+    if (error) {
+        [CMCommon showToastTitle:@"保存失败"];
+    }else{
+        [CMCommon showToastTitle:@"保存成功"];
+    
+    }
+    
+    NSLog(@"%@",contextInfo);
+    
+}
 @end
