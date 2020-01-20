77b1ef6b564b84b7e10763e0c36426892de39d97
ugtmac <syou66699@gmail.com>
2020-01-20 15:58:08 +0800

    no message

--- ios/AutoPacking/AutoPacking/AutoPackingVC.m
+++ ios/AutoPacking/AutoPacking/AutoPackingVC.m
@@ -35,7 +35,7 @@
         
         if (isPack) {
 
-            NSString *ids = @"c116";   // 站点编号
+            NSString *ids = @"c001";   // 站点编号
             BOOL willUpload = 1; // 打包后是否上传审核
             [self startPackingWithIds:ids willUpload:willUpload];
         }
