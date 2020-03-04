c56ec0a87598adce58f791c8aa9cb1523e7e8538
fish <gold.fish.a1b1@gmail.com>
2020-03-04 22:02:23 +0800

    更新了 优惠活动列表增加‘全部’分类； 修复了 优惠活动列表分类名重复导致报错bug；

--- js/rn/模板/默认/UGPromotionsController.tsx
+++ js/rn/模板/默认/UGPromotionsController.tsx
@@ -1,65 +1,74 @@
-import React, { Component } from "react";
-import { View, FlatList } from "react-native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import NetworkRequest1 from "../../网络/NetworkRequest1";
-import { UGPromoteModel } from "../../Model/常规/UGPromoteModel";
-import AppDefine from "../../通用/AppDefine";
-import { Button, Text } from "react-native-elements";
-import PromoteTableView from "./View/PromoteTableView";
+import React, {Component} from 'react';
+import {View, FlatList} from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import NetworkRequest1 from '../../网络/NetworkRequest1';
+import {UGPromoteModel} from '../../Model/常规/UGPromoteModel';
+import AppDefine from '../../通用/AppDefine';
+import {Button, Text} from 'react-native-elements';
+import PromoteTableView from './View/PromoteTableView';
+import objectPath from 'object-path';
 
 interface IProps {}
 interface IState {
-  dataArray: Array<{ category?: number; title: string; list: Array<UGPromoteModel> }>;
+  dataArray: Array<{category?: string; title: string; list: Array<UGPromoteModel>}>;
 }
 
 export default class UGPromotionsController extends Component<IProps, IState> {
   Tab = createMaterialTopTabNavigator();
-  style: "slide" | "popup" | "page" = "page"; // slide折叠、popup弹窗、page内页
+  style: 'slide' | 'popup' | 'page' = 'page'; // slide折叠、popup弹窗、page内页
   showTopBar = false;
 
   constructor(props) {
     super(props);
     this.state = {
-      dataArray: []
+      dataArray: [],
     };
   }
   componentDidMount() {
-    AppDefine.navController.setOptions({
-      title: "优惠活动",
-      headerStyle: { backgroundColor: AppDefine.themeColor },
+    AppDefine.navController?.setOptions({
+      title: '优惠活动',
+      headerStyle: {backgroundColor: AppDefine.themeColor},
       headerLeft: () => (
         <Button
-          icon={{ name: "chevron-left", size: 32, color: "white" }}
-          iconContainerStyle={{ marginLeft: -2 }}
-          buttonStyle={{ backgroundColor: "transparent" }}
+          icon={{name: 'chevron-left', size: 32, color: 'white'}}
+          iconContainerStyle={{marginLeft: -2}}
+          buttonStyle={{backgroundColor: 'transparent'}}
           onPress={() => {
             if (AppDefine.navController.canGoBack()) {
               AppDefine.navController.goBack();
             } else {
-              AppDefine.ocCall("UGNavigationController.current.popViewControllerAnimated:", [true]);
+              AppDefine.ocCall('UGNavigationController.current.popViewControllerAnimated:', [true]);
             }
           }}
         />
-      )
+      ),
     });
 
     NetworkRequest1.systeam_promotions().then(data => {
       this.style = data.style;
 
       if (data.showCategory) {
-        var temp: { [x: number]: Array<UGPromoteModel> } = [];
+        var temp: {[x: number]: Array<UGPromoteModel>} = [];
         data.list.map(pm => {
           var list = (temp[pm.category] = temp[pm.category] ?? []);
           list.push(pm);
         });
-        var dataArray = [];
+
+        var dataArray: Array<{category: string; title: string; list: Array<UGPromoteModel>}> = [];
+        dataArray.push({category: '0', title: '全部', list: data.list});
         for (var k in temp) {
-          dataArray.push({ category: k, title: data.categories[k] ?? "其他", list: temp[k] });
+          var title = data.categories[k] ?? '其他';
+          var obj = dataArray.objectWithValue(title, 'title');
+          if (obj) {
+            obj.list = obj.list.concat(temp[k]);
+          } else {
+            dataArray.push({category: k, title: title, list: temp[k]});
+          }
         }
         this.showTopBar = dataArray.length > 1;
-        this.setState({ dataArray: dataArray });
+        this.setState({dataArray: dataArray});
       } else {
-        this.setState({ dataArray: [{ title: "热门", list: data.list }] });
+        this.setState({dataArray: [{title: '热门', list: data.list}]});
       }
     });
   }
@@ -69,20 +78,19 @@ export default class UGPromotionsController extends Component<IProps, IState> {
       return null;
     }
     var contentViews = this.state.dataArray.map((plm, idx) => {
-      return <this.Tab.Screen name={plm.title} component={PromoteTableView} initialParams={{ list: plm.list, style: this.style }} key={idx} />;
+      return <this.Tab.Screen name={plm.title} component={PromoteTableView} initialParams={{list: plm.list, style: this.style}} key={idx} />;
     });
     return (
       <this.Tab.Navigator
         tabBarOptions={{
-          style: { backgroundColor: "transparent", height: this.showTopBar ? 50 : 0 },
-          labelStyle: { fontSize: 15 },
-          tabStyle: { width: 60 },
+          style: {backgroundColor: 'transparent', height: this.showTopBar ? 50 : 0},
+          labelStyle: {fontSize: 15},
+          tabStyle: {width: 60},
           scrollEnabled: true,
-          indicatorStyle: { marginBottom: 12, marginLeft: 10, height: 26, width: 42, borderRadius: 2, backgroundColor: AppDefine.themeColor },
-          inactiveTintColor: "#555",
-          activeTintColor: "white"
-        }}
-      >
+          indicatorStyle: {marginBottom: 12, marginLeft: 10, height: 26, width: 42, borderRadius: 2, backgroundColor: AppDefine.themeColor},
+          inactiveTintColor: '#555',
+          activeTintColor: 'white',
+        }}>
         {contentViews}
       </this.Tab.Navigator>
     );
--- js/rn/通用/AppDefine.ts
+++ js/rn/通用/AppDefine.ts
@@ -1,19 +1,20 @@
-import { Dimensions } from "react-native";
-import { NativeEventEmitter, NativeModules } from "react-native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import React from "react";
+import {Dimensions} from 'react-native';
+import {NativeEventEmitter, NativeModules} from 'react-native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React from 'react';
+import objectPath from 'object-path';
 
 type RootStackParamList = {
   Home: undefined;
-  Profile: { userId: string };
-  Feed: { sort: "latest" | "top" } | undefined;
+  Profile: {userId: string};
+  Feed: {sort: 'latest' | 'top'} | undefined;
 };
 
-export type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, "Profile">;
+export type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Profile'>;
 
 class OCFuncVariable {
-  vc: string = "";
-  ret: string = "";
+  vc: string = '';
+  ret: string = '';
 
   static init(): OCFuncVariable {
     var obj: any = {};
@@ -35,14 +36,14 @@ interface OCFuncModel {
 }
 
 export default class AppDefine {
-  static host = "http://接口域名"; // 接口域名
-  static siteId = "未知站点";
+  static host = 'http://接口域名'; // 接口域名
+  static siteId = '未知站点';
   static pageCount = 20;
-  static width = Dimensions.get("window").width;
-  static height = Dimensions.get("window").height;
+  static width = Dimensions.get('window').width;
+  static height = Dimensions.get('window').height;
   static statusBarHeight = 34;
   static bottomSafeHeight = 44;
-  static themeColor = "#609AC5";
+  static themeColor = '#609AC5';
 
   //
   static ocHelper = NativeModules.ReactNativeHelper; // oc助手
@@ -58,13 +59,13 @@ export default class AppDefine {
 
     // 优惠活动列表页
     pages.push({
-      vcName: "UGPromotionsController",
+      vcName: 'UGPromotionsController',
       fd_prefersNavigationBarHidden: true,
       允许游客访问: true,
-      允许未登录访问: true
+      允许未登录访问: true,
     });
 
-    AppDefine.ocCall("AppDefine.shared.setRnPageInfos:", [pages]);
+    AppDefine.ocCall('AppDefine.shared.setRnPageInfos:', [pages]);
   }
 
   static setup() {
@@ -72,17 +73,17 @@ export default class AppDefine {
     this.setRnPageInfo();
 
     // 监听原生发过来的事件通知
-    AppDefine.ocEvent.addListener("EventReminder", params => {
-      console.log("rn收到oc通知：");
+    AppDefine.ocEvent.addListener('EventReminder', params => {
+      console.log('rn收到oc通知：');
       console.log(params);
       var block = AppDefine.ocBlocks[params._EventName];
-      if (typeof block == "function") {
+      if (typeof block == 'function') {
         block(params.params);
       }
     });
 
     // 跳转到指定页面
-    AppDefine.ocEvent.addListener("SelectVC", params => {
+    AppDefine.ocEvent.addListener('SelectVC', params => {
       if (params.vcName) {
         // 退到root
         AppDefine.navController?.canGoBack() && AppDefine.navController?.popToTop();
@@ -93,15 +94,15 @@ export default class AppDefine {
     });
 
     // 移除页面
-    AppDefine.ocEvent.addListener("RemoveVC", params => {});
+    AppDefine.ocEvent.addListener('RemoveVC', params => {});
 
     // 设置接口域名
-    AppDefine.ocCall("AppDefine.shared.Host").then(host => {
+    AppDefine.ocCall('AppDefine.shared.Host').then(host => {
       AppDefine.host = host;
     });
 
     // 设置站点编号
-    AppDefine.ocCall("AppDefine.shared.SiteId").then(siteId => {
+    AppDefine.ocCall('AppDefine.shared.SiteId').then(siteId => {
       AppDefine.siteId = siteId;
     });
 
@@ -110,21 +111,21 @@ export default class AppDefine {
   }
 
   static ocCall(
-    selectors: string | ((vars: OCFuncVariable) => { [x: string]: OCFuncModel }),
+    selectors: string | ((vars: OCFuncVariable) => {[x: string]: OCFuncModel}),
     args1: Array<any | OCFuncModel> = [],
     args2: Array<any | OCFuncModel> = [],
-    args3: Array<any | OCFuncModel> = []
+    args3: Array<any | OCFuncModel> = [],
   ): Promise<any> {
     var array = [];
-    var temp: { [x: string]: OCFuncModel };
-    if (typeof selectors === "function") {
+    var temp: {[x: string]: OCFuncModel};
+    if (typeof selectors === 'function') {
       temp = selectors(OCFuncVariable.init());
     } else {
-      var sel: OCFuncModel = { selectors: selectors };
-      args1.length && (sel["args1"] = args1);
-      args2.length && (sel["args2"] = args2);
-      args3.length && (sel["args3"] = args3);
-      temp = { ret: sel };
+      var sel: OCFuncModel = {selectors: selectors};
+      args1.length && (sel['args1'] = args1);
+      args2.length && (sel['args2'] = args2);
+      args3.length && (sel['args3'] = args3);
+      temp = {ret: sel};
     }
     for (let [key, value] of Object.entries(temp)) {
       var obj = {};
@@ -140,36 +141,47 @@ export class NSValue {
   valueType: string;
   string: string;
 
-  constructor(valueType: "CGRect" | "CGPoint" | "CGSize" | "UIEdgeInsets" | "UIOffset" | "CGAffineTransform" | "CGVector", string: string) {
+  constructor(valueType: 'CGRect' | 'CGPoint' | 'CGSize' | 'UIEdgeInsets' | 'UIOffset' | 'CGAffineTransform' | 'CGVector', string: string) {
     this.valueType = valueType;
     this.string = string;
   }
 
   static CGRectMake(x: number, y: number, w: number, h: number): NSValue {
-    return new NSValue("CGRect", `{{${x}, ${y}}, {${w}, ${h}}}`);
+    return new NSValue('CGRect', `{{${x}, ${y}}, {${w}, ${h}}}`);
   }
 
   static CGPointMake(x: number, y: number): NSValue {
-    return new NSValue("CGPoint", `{{${x}, ${y}}}`);
+    return new NSValue('CGPoint', `{{${x}, ${y}}}`);
   }
 
   static CGSizeMake(w: number, h: number): NSValue {
-    return new NSValue("CGSize", `{{${w}, ${h}}}`);
+    return new NSValue('CGSize', `{{${w}, ${h}}}`);
   }
 
   static UIEdgeInsetsMake(top: number, left: number, bottom: number, right: number): NSValue {
-    return new NSValue("UIEdgeInsets", `{${top}, ${left}, ${bottom}, ${right}}`);
+    return new NSValue('UIEdgeInsets', `{${top}, ${left}, ${bottom}, ${right}}`);
   }
 
   static UIOffsetMake(horizontal: number, vertical: number): NSValue {
-    return new NSValue("UIOffset", `{${horizontal}, ${vertical}}`);
+    return new NSValue('UIOffset', `{${horizontal}, ${vertical}}`);
   }
 
   static CGAffineTransformMake(a: number, b: number, c: number, d: number, tx: number, ty: number): NSValue {
-    return new NSValue("CGAffineTransform", `[${a}, ${b}, ${c}, ${d}, ${tx}, ${ty}]`);
+    return new NSValue('CGAffineTransform', `[${a}, ${b}, ${c}, ${d}, ${tx}, ${ty}]`);
   }
 
   static CGVectorMake(dx: number, dy: number): NSValue {
-    return new NSValue("CGVector", `{${dx}, ${dy}}`);
+    return new NSValue('CGVector', `{${dx}, ${dy}}`);
   }
 }
+
+
+Array.prototype.objectWithValue = function(value: any, keyPath: string) {
+  for (var idx in this) {
+    var ele = this[idx];
+    if (objectPath.get(ele, keyPath) === value) {
+      return ele;
+    }
+  }
+  return null;
+};
