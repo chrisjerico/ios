12297c44af48e61cdae71e0c697249cb89de0030
ugtmac <syou66699@email.com>
2020-06-18 19:22:10 +0800

    c217 设置成B to   A

--- ug/Classes/Helper/FishUtility/define/AppDefine.m
+++ ug/Classes/Helper/FishUtility/define/AppDefine.m
@@ -395,7 +395,7 @@
 
 
 - (BOOL)isBA {
-    return [@"c001,c085,c208,a002,c054,c212,c200,c213,c134,c092,c116" containsString:_SiteId];
+    return [@"c001,c085,c208,a002,c054,c212,c200,c213,c134,c092,c116,c217" containsString:_SiteId];
 }
 
 
