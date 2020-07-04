7020266a30d24935eb697bd58bb73d92c047393b
ugtmac <syou66699@email.com>
2020-07-04 21:05:20 +0800

    打包c205,https://center001.fhptbet.net/gd.php?action=view&id=117518 117518】c217】原生ios APP进入cq9电子显示下图错误提示

--- ug/Classes/Helper/FishUtility/define/AppDefine.m
+++ ug/Classes/Helper/FishUtility/define/AppDefine.m
@@ -10,7 +10,7 @@
 
 
 
-#define __SiteID__ @"c213"
+#define __SiteID__ @"c217"
 #define LocalRnVersion @"1.4.66"
 
 
@@ -144,7 +144,7 @@
         _Test = true;
         _SiteId = [[NSUserDefaults standardUserDefaults] stringForKey:@"当前站点Key"];
         if (!_SiteId.length) {
-            _SiteId = @"test10";
+            _SiteId = @"c217";
         }
 #endif
         NSLog(@"%@",[_allSites objectWithValue:_SiteId.lowercaseString keyPath:@"siteId"]);
