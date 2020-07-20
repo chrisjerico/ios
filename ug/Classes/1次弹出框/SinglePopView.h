adfce66b3a935bfec07bb8f5e0ff8e02b7c11f42
andrew <1@1.com>
2020-07-20 20:09:25 +0800

    做弹出框

--- ug/Classes/1次弹出框/SinglePopView.h
+++ ug/Classes/1次弹出框/SinglePopView.h
@@ -11,8 +11,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SinglePopView : UIView
-@property (weak, nonatomic) IBOutlet mYYLabel *yyLabel;
 
+@property (strong, nonatomic)  NSString *content;
 + (SinglePopView *) sharedInstance;
 @end
 
--- ug/Classes/1次弹出框/SinglePopView.m
+++ ug/Classes/1次弹出框/SinglePopView.m
@@ -11,7 +11,7 @@
 @interface SinglePopView()
 
 @property (strong, nonatomic) IBOutlet UIView *contentView;
-
+@property (weak, nonatomic) IBOutlet mYYLabel *yyLabel;
 
 @end
 
@@ -44,5 +44,35 @@ static SinglePopView *singlePopView = nil;
 
 - (IBAction)closeBtnClick:(id)sender {
     [SGBrowserView hide];
+}
+
+-(void)setContent:(NSString *)content{
+    _content = content;
+     NSMutableAttributedString *text = [[NSMutableAttributedString alloc]initWithString:content];
+        text.yy_font = [UIFont systemFontOfSize:17];
+        NSError *error;
+        NSDataDetector *dataDetector=[NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink error:&error];
+        NSArray *arrayOfAllMatches=[dataDetector matchesInString:wholeText options:NSMatchingReportProgress range:NSMakeRange(0, wholeText.length)];
+        //NSMatchingOptions匹配方式也有好多种，我选择NSMatchingReportProgress，一直匹配
+    
+      //我们得到一个数组，这个数组中NSTextCheckingResult元素中包含我们要找的URL的range，当然可能找到多个URL，找到相应的URL的位置，用YYlabel的高亮点击事件处理跳转网页
+        for (NSTextCheckingResult *match in arrayOfAllMatches)
+        {
+            //        NSLog(@"%@",NSStringFromRange(match.range));
+            [text yy_setTextHighlightRange:match.range//设置点击的位置
+                                     color:[UIColor orangeColor]
+                           backgroundColor:[UIColor whiteColor]
+                                 tapAction:^(UIView *containerView, NSAttributedString *text, NSRange range, CGRect rect){
+                                     NSLog(@"这里是点击事件");
+                                     //跳转用的WKWebView
+                                     WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+                                     [self.view addSubview:webView];
+                                     [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[wholeText substringWithRange:match.range]]]];
+                                     
+                                 }];
+        }
+        self.yyLabel.attributedText = text;
+   
+ 
 }
 @end
