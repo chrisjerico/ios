e7c70fda606a890e460666e79f97856af3a9c3b1
andrew <1@1.com>
2020-08-24 13:53:17 +0800

    做下注明细界面

---
+++ ug/Classes/Lottery/View/越南/越南下注明细View/YNBetDetailView.h
@@ -0,0 +1,17 @@
+//
+//  YNBetDetailView.h
+//  UGBWApp
+//
+//  Created by ug on 2020/8/24.
+//  Copyright © 2020 ug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YNBetDetailView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ug/Classes/Lottery/View/越南/越南下注明细View/YNBetDetailView.m
@@ -0,0 +1,21 @@
+//
+//  YNBetDetailView.m
+//  UGBWApp
+//
+//  Created by ug on 2020/8/24.
+//  Copyright © 2020 ug. All rights reserved.
+//
+
+#import "YNBetDetailView.h"
+
+@implementation YNBetDetailView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
