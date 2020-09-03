1531b267f2ca5cf574c3dcc6a9d74444bf88b8cb
andrew <1@1.com>
2020-09-03 15:01:14 +0800

    做开奖详情

---
+++ ug/Classes/Lottery/View/越南/越南开奖详情/河内/YNHLPrizeDetailView.m
@@ -0,0 +1,21 @@
+//
+//  YNHLPrizeDetailView.m
+//  UGBWApp
+//
+//  Created by ug on 2020/9/3.
+//  Copyright © 2020 ug. All rights reserved.
+//
+
+#import "YNHLPrizeDetailView.h"
+
+@implementation YNHLPrizeDetailView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
