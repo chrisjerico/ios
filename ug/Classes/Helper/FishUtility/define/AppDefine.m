fb5399b5e27ee0cf4869417d0dffcd1a15665f74
xionghx <xionghuanxin@hotmail.com>
2020-09-17 18:36:53 +0800

    编号：124761 标题：IOS】】原生苹果 接口域名换到这个 http://370311.com/ 【普通】 优先级：1 站点：c006

--- ug/Classes/Helper/FishUtility/define/AppDefine.m
+++ ug/Classes/Helper/FishUtility/define/AppDefine.m
@@ -146,7 +146,7 @@
         _SiteId = [[NSUserDefaults standardUserDefaults] stringForKey:@"当前站点Key"];
         if (!_SiteId.length) {
 //			_SiteId = @"test61f";
-			_SiteId = @"c126";
+			_SiteId = @"c006";
 
         }
 
