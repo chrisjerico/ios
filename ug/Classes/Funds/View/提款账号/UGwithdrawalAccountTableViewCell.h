b4aefa15056e0858b06312acdb28ddfa06b119a4
andrew <2@2.com>
2020-09-28 16:57:20 +0800

    做虚拟币

---
+++ ug/Classes/Funds/View/提款账号/UGwithdrawalAccountTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  UGwithdrawalAccountTableViewCell.h
+//  UGBWApp
+//
+//  Created by fish on 2020/9/28.
+//  Copyright © 2020 ug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UGwithdrawalAccountTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
