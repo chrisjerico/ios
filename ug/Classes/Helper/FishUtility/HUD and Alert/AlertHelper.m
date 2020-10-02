834277e86290f000bb62e8bd0de8c76f9213df2d
fish <gold.fish.a1b1@gmail.com>
2020-10-02 17:21:29 +0800

    修复上一个提交产生的bug。

--- ug/Classes/Helper/FishUtility/HUD and Alert/AlertHelper.m
+++ ug/Classes/Helper/FishUtility/HUD and Alert/AlertHelper.m
@@ -14,17 +14,25 @@
 
 + (UIAlertController *)showAlertView: (NSString *)title msg:(NSString *)msg btnTitles:(NSArray *)btnTitles {
     UIAlertController *ac = [UIAlertController alertWithTitle:title msg:msg btnTitles:btnTitles];
-    [NavController1.presentedViewController dismissViewControllerAnimated:false completion:^{
+    if (NavController1.presentedViewController) {
+        [NavController1.presentedViewController dismissViewControllerAnimated:false completion:^{
+            [NavController1 presentViewController:ac animated:true completion:nil];
+        }];
+    } else {
         [NavController1 presentViewController:ac animated:true completion:nil];
-    }];
+    }
     return ac;
 }
 
 + (UIAlertController *)showActionSheet:(NSString *)title msg:(NSString *)msg btnTitles:(NSArray *)btnTitles cancel:(NSString *)cancel {
     UIAlertController *ac = [UIAlertController actionSheetWithTitle:title msg:msg btnTitles:btnTitles cancel:(NSString *)cancel];
-    [NavController1.presentedViewController dismissViewControllerAnimated:false completion:^{
+    if (NavController1.presentedViewController) {
+        [NavController1.presentedViewController dismissViewControllerAnimated:false completion:^{
+            [NavController1 presentViewController:ac animated:true completion:nil];
+        }];
+    } else {
         [NavController1 presentViewController:ac animated:true completion:nil];
-    }];
+    }
     return ac;
 }
 
