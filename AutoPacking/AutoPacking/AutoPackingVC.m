520ca1cfc5832e81c6b91fe41e79ec129d62fd0c
andrew <2@2.com>
2020-10-12 17:06:44 +0800

    编号：	126933 标题：	IOS】B站更换下图推荐收益部分的图片，图片还在制作中，稍后制作完成放置工单内 【普通】

--- AutoPacking/AutoPacking/AutoPackingVC.m
+++ AutoPacking/AutoPacking/AutoPackingVC.m
@@ -57,7 +57,7 @@
 
                 
 
-                NSString *ids = @"c245";    // 站点编号(可以批量打包用','号隔开)  c175  c008 c049
+                NSString *ids = @"c126b";    // 站点编号(可以批量打包用','号隔开)  c175  c008 c049
                 NSString *branch = @"dev_master";// 分支名
                 BOOL isForce = false;      // 是否强制更新
                 NSString *updateLog = @""; // 更新日志，给用户看的
