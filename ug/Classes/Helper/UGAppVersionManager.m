0e888ed2e23711addbf3d5bc15d64c8727d4c08f
fish <gold.fish.a1b1@gmail.com>
2020-11-01 17:31:48 +0800

    关闭更新提示后，每次重启app时会再次跳出版本更新提示；

--- ug/Classes/Helper/UGAppVersionManager.m
+++ ug/Classes/Helper/UGAppVersionManager.m
@@ -118,7 +118,8 @@ static CGFloat PromptInterval = 15 * 24 * 3600;     // 新版本提示间隔
                 }];
             }
         }
-        else if (fabs([[NSDate date] timeIntervalSinceDate:[[NSUserDefaults standardUserDefaults] objectForKey:kLastPromptDate]]) > PromptInterval) {
+        else {
+//        else if (fabs([[NSDate date] timeIntervalSinceDate:[[NSUserDefaults standardUserDefaults] objectForKey:kLastPromptDate]]) > PromptInterval) {
             showAlert();
         }
     }
