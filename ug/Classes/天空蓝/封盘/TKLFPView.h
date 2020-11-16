149678ae4dc89eb0c124184354ef75a26862fcde
andrew <2@2.com>
2020-11-16 15:15:37 +0800

    11

---
+++ ug/Classes/天空蓝/封盘/TKLFPView.h
@@ -0,0 +1,17 @@
+//
+//  TKLFPView.h
+//  UGBWApp
+//
+//  Created by andrew on 2020/11/16.
+//  Copyright © 2020 ug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TKLFPView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ug/Classes/天空蓝/封盘/TKLFPView.m
@@ -0,0 +1,21 @@
+//
+//  TKLFPView.m
+//  UGBWApp
+//
+//  Created by andrew on 2020/11/16.
+//  Copyright © 2020 ug. All rights reserved.
+//
+
+#import "TKLFPView.h"
+
+@implementation TKLFPView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
