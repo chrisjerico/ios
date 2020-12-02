d0ceab8a5f3718331068bb23d5015bd8411f69c9
andrew <1@1.com>
2020-12-02 20:29:39 +0800

    编号：	122190 标题：	IOS】c053 原生APP 投注棋牌 前台不显示注单 手机端H5正常显示，麻烦技术评估给原生添加注单回传，谢谢 【普通】

--- ug/Classes/Mine/Model/UGBetsRecordListModel.h
+++ ug/Classes/Mine/Model/UGBetsRecordListModel.h
@@ -48,6 +48,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) NSString *winCount;   /**<   中奖笔数 */
 //@property (nonatomic, strong) NSString *winAmount;   /**<   中奖金额 */
 @property (nonatomic, strong) NSString *winLoseAmount;   /**<  输赢 */
+@property (nonatomic, strong) NSString *ticketNo;        /**<  单号*/
 //==========下注明细
 @property (nonatomic, strong) NSString *title;           /**<  彩種  */
 @property (nonatomic, strong) NSString *betCount;        /**<  笔数*/
